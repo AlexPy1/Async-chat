332eb9a436b52a8dedd34a7c07703a20b24921bd
AlexPy1 <79162541486@yandex.ru>
2022-10-15 12:39:47 +0300

    Lesson 2 fix

--- lesson_2_3.py
+++ lesson_2_3.py
@@ -1,8 +1,9 @@
 import yaml
+
 data = {
     'list': ['one', 'two'],
     'int': 2,
     'dict': {'int_with_uni_1': '1€', 'int_with_uni_2': '2€'}
 }
-with open('result.yaml', 'w') as f:
-    yaml.dump(data,f, default_flow_style=True, allow_unicode = True)
+with open('result.yaml', 'w', encoding='utf-8') as f:
+    yaml.dump(data, f, default_flow_style=True, allow_unicode=True)
